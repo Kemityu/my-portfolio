5a470c8a75af426906d6be41c938fa8af781403f
kemtiyu <yapimaruis55@gmail.com>
2025-02-28 12:02:42 +0000

    ajout d'une experience professionnelle

---
+++ pages/classe.py
@@ -0,0 +1,13 @@
+class Voiture:
+    marque = "lamborghini"
+    couleur = "rouge"
+
+voiture_01 = Voiture()
+voiture_02 = Voiture()
+#print(voiture_01.couleur)
+#print(voiture_02.couleur)
+
+voiture_01.couleur = "bleu"
+voiture_02.couleur = "vert"
+print(voiture_01.couleur)
+print(voiture_02.couleur)
